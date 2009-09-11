89adc71078b064d6ef7ced2309e887fe023a7014
Jurgen J. Vinju <jurgen.vinju@cwi.nl>
2009-09-11 08:33:50 +0000

    The list used for the fast and shared implementation contained a bug which caused the sublist to return incorrect results under certain circumstances.
    The  patch by Arnold Lankamp resolves the problem. This solves Bug #289064

--- src/org/eclipse/imp/pdb/test/BaseTestList.java
+++ src/org/eclipse/imp/pdb/test/BaseTestList.java
@@ -164,5 +164,70 @@ public abstract class BaseTestList extends TestCase {
 			}
 		}
 	}
-
+	
+	// NOTE: This is not a very good test, but sufficient for it's purpose.
+	public void testSubList(){
+		// Front
+		IListWriter flw = vf.listWriter(tf.integerType());
+		for(int i = 0; i < 20; i++){
+			flw.append(vf.integer(i));
+		}
+		IList fList = flw.done();
+		
+		// Back
+		IListWriter blw = vf.listWriter(tf.integerType());
+		for(int i = 19; i >= 0; i--){
+			blw.insert(vf.integer(i));
+		}
+		IList bList = blw.done();
+		
+		// Overlap
+		IListWriter olw = vf.listWriter(tf.integerType());
+		for(int i = 9; i >= 0; i--){
+			olw.insert(vf.integer(i));
+		}
+		for(int i = 10; i < 20; i++){
+			olw.append(vf.integer(i));
+		}
+		IList oList = olw.done();
+		
+		IList fSubList = fList.sublist(0, 5);
+		IList bSubList = bList.sublist(0, 5);
+		IList oSubList = oList.sublist(0, 5);
+		checkSubListEquality(fSubList, bSubList, oSubList);
+		
+		fSubList = fList.sublist(1, 5);
+		bSubList = bList.sublist(1, 5);
+		oSubList = oList.sublist(1, 5);
+		checkSubListEquality(fSubList, bSubList, oSubList);
+		
+		fSubList = fList.sublist(0, 15);
+		bSubList = bList.sublist(0, 15);
+		oSubList = oList.sublist(0, 15);
+		checkSubListEquality(fSubList, bSubList, oSubList);
+		
+		fSubList = fList.sublist(1, 15);
+		bSubList = bList.sublist(1, 15);
+		oSubList = oList.sublist(1, 15);
+		checkSubListEquality(fSubList, bSubList, oSubList);
+		
+		fSubList = fList.sublist(5, 5);
+		bSubList = bList.sublist(5, 5);
+		oSubList = oList.sublist(5, 5);
+		checkSubListEquality(fSubList, bSubList, oSubList);
+		
+		fSubList = fList.sublist(5, 10);
+		bSubList = bList.sublist(5, 10);
+		oSubList = oList.sublist(5, 10);
+		checkSubListEquality(fSubList, bSubList, oSubList);
+		
+		fSubList = fList.sublist(15, 5);
+		bSubList = bList.sublist(15, 5);
+		oSubList = oList.sublist(15, 5);
+		checkSubListEquality(fSubList, bSubList, oSubList);
+	}
+	
+	private static void checkSubListEquality(IList fList, IList bList, IList oList){
+		if(!fList.isEqual(bList) || !bList.isEqual(oList)) fail("IList#subList is broken: "+fList+" "+bList+" "+oList);
+	}
 }
